03f4bd6e70a1afbdf3ef34429f4ec1f08487fcde
luxpmsof <84482391+luxpmsof@users.noreply.github.com>
2021-07-07 16:37:29 +0900

    ReactGA.initialize('UA-200356276-1')

--- website-updated/luxpm-website/pages/_app.js
+++ website-updated/luxpm-website/pages/_app.js
@@ -6,10 +6,17 @@ import 'react-accessible-accordion/dist/fancy-example.css'
 import '../node_modules/react-modal-video/css/modal-video.min.css'
 import '../public/css/style.css'
 import '../public/css/responsive.css'
+import ReactGA from 'react-ga';
 
 import Layout from '../component/layout';
+import React, {useEffect} from "react";
 
 const MyApp =  ({ Component, pageProps }) => {
+    React.useEffect(() => {
+        ReactGA.initialize('UA-200356276-1')
+        ReactGA.set({ page: window.location.pathname });
+        ReactGA.pageview(window.location.pathname);
+    }, [])
 
   return (
       <Layout>
